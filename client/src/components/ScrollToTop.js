eb15f07b52b61171c2c0ec60231a400e01a5ef39
Umarjon <Umarjonn.sharofiddinov07@mail.ru>
2021-09-30 13:21:43 +0300

    Update ScrollToTop.js

--- client/src/components/ScrollToTop.js
+++ client/src/components/ScrollToTop.js
@@ -3,10 +3,7 @@ import { useLocation } from "react-router-dom";
 
 function ScrollToTop() {
   const { pathname } = useLocation();
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-  return null;
+  useEffect(() => {window.scrollTo(0, 0);}, [pathname]); return null;
 }
 
 export default ScrollToTop;
